94e4a28e670f83350dc0e666af6b0eae0bf77330
oluwapaso <adebayoishola01@gmail.com>
2026-09-14 12:11:07 +0100

    Fixing footers

--- components/footers/FooterVar-1.tsx
+++ components/footers/FooterVar-1.tsx
@@ -13,7 +13,9 @@ import { RootState } from '@/app/GlobalRedux/store';
 import Link from 'next/link';
 import CustomLinkMain from '../CustomLink';
 import { BiLayerPlus, BiRefresh, BiTrash } from 'react-icons/bi';
+import { Helpers } from '@/_lib/helper';
 
+const helpers = new Helpers();
 const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, raw_data?: any }) => {
 
     const theme = useSelector((state: RootState) => state.theme);
@@ -106,14 +108,14 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
             <footer className={`w-full relative bg-gray-900 py-20 items-center justify-center text-gray-100`}>
 
                 <div className='container grid grid-cols-12 gap-12 mx-auto'>
-                    <div className=' col-span-4 flex flex-col'>
+                    <div className='col-span-3 flex flex-col'>
                         <div className='mb-4'>
                             <div className="font-medium text-2xl h-[55px]">
                                 <Image src={`${themeSett?.light_logo || "/Houxera-logo-white.png"}`} height={50} width={150} className="" alt={`Houxera MLS and IDX provider in Nieria/Africa`} />
                             </div>
                         </div>
 
-                        <div className='text-sm leading-7'>
+                        <div className='text-sm leading-7 text-zinc-400'>
                             {themeSett?.footer_note || `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                             Non temporibus hic sunt iure magnam labore,
                             unde tenetur totam quam porro veritatis
@@ -121,7 +123,7 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
                         </div>
 
                         <div className='mt-6'>
-                            <div className=' flex flex-col space-y-2.5'>
+                            <div className=' flex flex-col space-y-2.5 text-zinc-400'>
                                 <div className=' flex items-start space-x-1.5'>
                                     <FaMapMarkerAlt size={14} className='shrink-0' />
                                     <span className=' flex flex-col -mt-1'>
@@ -133,12 +135,20 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
 
                                 <div className=' flex items-center space-x-1.5'>
                                     <PiPhoneIncoming size={14} />
-                                    <span>{brker_info?.contact_info?.phone_cell}</span>
+                                    <span>
+                                        <Link href={`tel:${brker_info?.contact_info?.phone_cell}`} className={`hover:text-${themeSett.primary_color}`}>
+                                            {brker_info?.contact_info?.phone_cell}
+                                        </Link>
+                                    </span>
                                 </div>
 
                                 <div className=' flex items-center space-x-1.5'>
                                     <BiEnvelopeOpen size={14} />
-                                    <span>{brker_info?.email}</span>
+                                    <span>
+                                        <Link href={`mailto:${brker_info?.email}`} className={`hover:text-${themeSett.primary_color}`}>
+                                            {brker_info?.email}
+                                        </Link>
+                                    </span>
                                 </div>
                             </div>
 
@@ -146,31 +156,36 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
                                 *:flex *:items-center *:justify-center *:cursor-pointer'>
 
                                 {brker_info?.social_accounts?.facebook &&
-                                    <Link href={`${brker_info?.social_accounts?.facebook}`} target='_blank' >
+                                    <Link href={`${brker_info?.social_accounts?.facebook}`} target='_blank'
+                                        className={`hover:bg-${themeSett.primary_color} text-${themeSett.primary_button_text}`}>
                                         <FaFacebook size={20} />
                                     </Link>
                                 }
 
                                 {brker_info?.social_accounts?.twitter &&
-                                    <Link href={`${brker_info?.social_accounts?.twitter}`} target='_blank' >
+                                    <Link href={`${brker_info?.social_accounts?.twitter}`} target='_blank'
+                                        className={`hover:bg-${themeSett.primary_color} text-${themeSett.primary_button_text}`}>
                                         <BsTwitterX size={20} />
                                     </Link>
                                 }
 
                                 {brker_info?.social_accounts?.linkedin &&
-                                    <Link href={`${brker_info?.social_accounts?.linkedin}`} target='_blank' >
+                                    <Link href={`${brker_info?.social_accounts?.linkedin}`} target='_blank'
+                                        className={`hover:bg-${themeSett.primary_color} text-${themeSett.primary_button_text}`}>
                                         <LiaLinkedin size={20} />
                                     </Link>
                                 }
 
                                 {brker_info?.social_accounts?.youtube &&
-                                    <Link href={`${brker_info?.social_accounts?.youtube}`} target='_blank' >
+                                    <Link href={`${brker_info?.social_accounts?.youtube}`} target='_blank'
+                                        className={`hover:bg-${themeSett.primary_color} text-${themeSett.primary_button_text}`}>
                                         <FaYoutube size={20} />
                                     </Link>
                                 }
 
                                 {brker_info?.social_accounts?.whatsapp &&
-                                    <Link href={`https://api.whatsapp.com/send/?phone=${brker_info?.social_accounts?.whatsapp}`} target='_blank' >
+                                    <Link href={`https://api.whatsapp.com/send/?phone=${brker_info?.social_accounts?.whatsapp}`} target='_blank'
+                                        className={`hover:bg-${themeSett.primary_color} text-${themeSett.primary_button_text}`}>
                                         <BsWhatsapp size={20} />
                                     </Link>
                                 }
@@ -178,14 +193,13 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
                         </div>
                     </div>
 
-                    <div className=' col-span-8 grid grid-cols-3 gap-12 *:flex *:flex-col'>
-
+                    <div className='col-span-6 grid grid-cols-2 gap-12 *:flex *:flex-col'>
                         {(Array.isArray(themeSett.footer_menu) && themeSett.footer_menu.length > 0) ? (
                             themeSett.footer_menu.map((menu: any, index: any) => {
 
                                 return (
                                     <div key={index} className=''>
-                                        <div className='mb-4 font-semibold text-xl border-b-3 border-gray-100 w-fit h-[55px]'>
+                                        <div className='mb-4 font-semibold text-zinc-200 ext-xl border-b-3 border-gray-100 w-fit h-[55px]'>
                                             {menu.title}
                                         </div>
 
@@ -194,8 +208,8 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
                                             *:flex *:items-center *:space-x-1.5'>
                                                 {menu.sub_menu.map((sub_menu: any, sub_index: any) => {
                                                     return <CustomLinkMain key={sub_index} href={`${sub_menu.link ? menu.link : ""}`} is_theme={is_theme}
-                                                        className={`ease-in py-2 hover:delay-150 hover:bg-${themeSett.primary_color} 
-                                                        hover:text-${themeSett.primary_button_text} transition-all rounded
+                                                        className={`w-fit text-zinc-400 ease-in py-2 hover:delay-150 hover:bg-${themeSett.primary_color} 
+                                                        hover:text-${themeSett.primary_button_text} transition-all rounded 
                                                         hover:px-2 hover:py-2 cursor-pointer`}>
                                                         <FaArrowRightLong size={13} />
                                                         <span>{sub_menu.title}</span>
@@ -207,14 +221,44 @@ const FooterVar1 = ({ is_theme = false, raw_data = {} }: { is_theme?: boolean, r
                                 )
                             })
                         ) : null}
-
                     </div>
 
+                    <div className='col-span-3 flex flex-col'>
+                        {/* Newsletter */}
+                        {!themeSett.show_newsletter &&
+                            <div className="space-y-6">
+                                <h3 className="text-sm font-semibold uppercase tracking-wider text-zinc-100">
+                                    {themeSett.newsletter_header ? themeSett.newsletter_header : "Newsletter"}
+                                </h3>
+                                <p className="text-sm text-zinc-400">
+                                    {themeSett.newsletter_sub_header
+                                        ? themeSett.newsletter_sub_header
+                                        : "Subscribe to our newsletter for the latest updates and insights."
+                                    }
+                                </p>
+                                <div className="space-y-3">
+                                    <input type="email" placeholder="Enter your email"
+                                        className="w-full px-4 py-2 border-zinc-700 bg-zinc-800 text-zinc-100 placeholder:text-zinc-500 focus-visible:ring-blue-500"
+                                    />
+                                    <button className={`w-full px-3 py-2 rounded cursor-pointer bg-${themeSett.primary_color} 
+                                    text-${themeSett.primary_button_text} hover:bg-${helpers.adjustColorShade(themeSett.primary_color, 1)}`}>
+                                        {themeSett.newsletter_btn_text ? themeSett.newsletter_btn_text : "Subscribe"}
+                                    </button>
+                                </div>
+                            </div>
+                        }
+                    </div>
                 </div>
 
+                <div className='container mx-auto mt-16 border-t border-gray-700 pt-8 flex items-center justify-between'>
+                    <div className="flex flex-wrap items-center justify-end gap-6 text-sm text-zinc-500">
+                        <CustomLinkMain href="/privacy-policy" className="hover:text-zinc-300">Privacy Policy</CustomLinkMain>
+                        <CustomLinkMain href="/terms" className="hover:text-zinc-300">Terms of Service</CustomLinkMain>
+                    </div>
 
-                <div className='container mx-auto mt-16 border-t border-gray-700 pt-8 flex items-center justify-center'>
-                    &copy; {new Date().getFullYear()}. All rights reserved. Made by Houxera
+                    <div className='text-zinc-400'>
+                        &copy; {new Date().getFullYear()}. All rights reserved. Made by Houxera
+                    </div>
                 </div>
 
                 {/* ${showButtons ? "fixed" : "hidden"}  */}
