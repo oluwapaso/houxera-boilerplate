763e94eaaffa706bf5bc72f5b1067441903f4a37
oluwapaso <adebayoishola01@gmail.com>
2026-09-16 07:52:26 +0100

    Fixing navs

--- components/navs/MobileSubMenuContaier.tsx
+++ components/navs/MobileSubMenuContaier.tsx
@@ -8,14 +8,14 @@ const MobileSubMenuContaier = ({ menu, themeSett, is_theme }: { menu: any, theme
 
     return <div className={`relative flex flex-col hover:border-b-${themeSett.primary_color} transition-all ease-in hover:delay-150 whitespace-nowrap`}>
         <div className={`font-semibold`}>{menu.title}</div>
-        <div className='mt-2 w-full flex flex-col *:space-x-1.5 *:flex *:items-center *:py-4 *:w-full divide-y divide-gray-200 '>
+        <div className='mt-2 w-full flex flex-col *:space-x-2.5 *:flex *:items-center *:py-4 *:w-full divide-y divide-gray-200 '>
             {(Array.isArray(menu.sub_menu) && menu.sub_menu.length > 0) && (
                 menu.sub_menu.map((sub_menu: any, index: any) => {
                     return <CustomLinkMain key={index} href={`${sub_menu.link ? menu.link : ""}`} is_theme={is_theme}
-                        className={` text-gray-900 hover:bg-${themeSett.primary_color} hover:text-white transition-all 
+                        className={`hover:bg-${themeSett.primary_color} hover:text-white transition-all 
                         ease-in hover:delay-150`}>
                         <FaArrowRightLong size={18} className='shrink-0 text-gray-600' />
-                        <span>{sub_menu.title}</span>
+                        <span className=' text-gray-800'>{sub_menu.title}</span>
                     </CustomLinkMain>
                 })
             )}
