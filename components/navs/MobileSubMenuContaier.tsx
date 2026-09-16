6f52ebd71c1756026bf56e32da7df11662f1f911
oluwapaso <adebayoishola01@gmail.com>
2026-09-16 07:09:05 +0100

    Fixing navs

--- components/navs/MobileSubMenuContaier.tsx
+++ components/navs/MobileSubMenuContaier.tsx
@@ -2,13 +2,15 @@
 
 import React, { useEffect, useRef, useState } from 'react'
 import CustomLinkMain from '../CustomLink'
+import { FaArrowRightLong } from 'react-icons/fa6'
 
 const MobileSubMenuContaier = ({ menu, themeSett, is_theme }: { menu: any, themeSett: any, is_theme: boolean }) => {
 
     return <div className={`relative flex flex-col hover:border-b-${themeSett.primary_color} transition-all ease-in hover:delay-150 whitespace-nowrap`}>
-        <div className={` `}>{menu.title}</div>
-        <div className='pl-5 mt-2'>
-            <div className=' w-full flex flex-col *:flex *:py-4 *:w-full divide-y divide-gray-200 '>
+        <div className={`font-semibold`}>{menu.title}</div>
+        <div className='mt-2 flex space-x-1.5'>
+            <FaArrowRightLong size={18} className='shrink-0' />
+            <div className='grow flex flex-col *:flex *:py-4 *:w-full divide-y divide-gray-200 '>
                 {(Array.isArray(menu.sub_menu) && menu.sub_menu.length > 0) && (
                     menu.sub_menu.map((sub_menu: any, index: any) => {
                         return <CustomLinkMain key={index} href={`${sub_menu.link ? menu.link : ""}`} is_theme={is_theme}
