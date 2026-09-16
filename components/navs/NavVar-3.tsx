51451b9a4f093a1f142b066b077f373528c80d3e
oluwapaso <adebayoishola01@gmail.com>
2026-09-16 07:24:56 +0100

    Fixing navs

--- components/navs/NavVar-3.tsx
+++ components/navs/NavVar-3.tsx
@@ -170,8 +170,9 @@ const NavVar3 = ({ transparent = true, is_theme = false, raw_data = {} }: { tran
                     </div>
 
                     {isMenuOpen &&
-                        <div className=' absolute w-full max---w-[90%] top-20 right-0 bg-white shadow-xl flex flex-col *:flex *:px-5 *:py-4 
-                            divide-y divide-gray-200 rounded-b-md max-h-[calc(100dvh-80px)] border border-red-600 overflow-y-auto'>
+                        <div className={` absolute w-full top-20 right-0 bg-white shadow-xl flex flex-col *:flex *:px-5 *:py-4 
+                            divide-y divide-gray-200 rounded-b-md overflow-y-auto
+                            ${isScrolled ? "max-h-[calc(100dvh-96px)]" : "max-h-[calc(100dvh-80px)]"}  `}>
                             {(Array.isArray(themeSett.top_menu) && themeSett.top_menu.length > 0) ? (
                                 themeSett.top_menu.map((menu: any, index: any) => {
 
